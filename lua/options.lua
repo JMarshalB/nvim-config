e36a3deb3ae99c66af9727bfbfe6158f41fd58d8
Jaylen Brown <jbrown@tritonhealthcare.net>
2026-02-15 19:37:36 -0500

    Updated configs

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,11 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Folding
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldlevel = 99
+
+-- Relative line numbers
+vim.o.relativenumber = true
