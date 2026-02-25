dc49ffeac74de745a236ee864d6a47fc290f3b72
Jaylen Brown <jbrown@tritonhealthcare.net>
2026-02-24 20:43:39 -0500

    Added claude code intergration and neogit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,48 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<Esc>", "<cmd>noh<cr>", { desc = "Clear search highlight" })
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+vim.keymap.set("n", "<leader>rr", function()
+  -- require("nvchad.term").runner {
+  --    id = "htoggleTerm",
+  --    pos = "sp",
+  --
+  --    cmd = function()
+  --      local file = vim.fn.expand "%"
+  --
+  --      local ft_cmds = {
+  --        python = "python3 " .. file,
+  --        cpp = "clear && g++ -o out " .. file .. " && ./out",
+  --      }
+  --
+  --      return ft_cmds[vim.bo.ft]
+  --    end,
+  -- }
+
+
+  local python = require("venv-selector").python()
+
+  if not python then
+    vim.notify("No venv selected. Use :VenvSelect first.", vim.log.levels.WARN)
+    return
+  end
+
+  local file = vim.fn.expand("%:p")
+
+  if vim.bo.filetype ~= "python" then
+    vim.notify("Not a Python file.", vim.log.levels.WARN)
+    return
+  end
+
+  -- Save the file first, then run it in a terminal split
+  vim.cmd("w")
+  require("nvchad.term").runner { pos = "sp", id = "htoggleTerm", cmd = python .. " " .. file}
+  
+  vim.cmd("stopinsert")
+  
+end, { desc = "Run Python file with selected venv" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,6 +35,49 @@ return {
     keys = {
       { "<leader>pv", "<cmd>VenvSelect<cr>", desc = "Select Python venv" },
     },
-    opts = {},
+    opts = {}
   },
+
+  {
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    opts = {
+      auto_start = true,
+      terminal = {
+        provider = "none",
+      },
+    },
+    config = true,
+    keys = {
+      { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+      },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+    },
+  },
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit cwd=%:p:h<cr>", desc = "Show Neogit UI" }
+    }
+  },
+
 }
